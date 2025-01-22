d33b56292e63d8d3fbfa28c603f98524028b480f
Arif <arif15-12301@diu.edu.bd>
2025-01-22 11:33:43 +0600

    wip

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,1 @@
+<h4>Hello Category</h4>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('categories', [CategoryController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductController::class);
