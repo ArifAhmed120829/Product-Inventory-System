b91c2a1e209223a59ad797c6302df02f1756ca56
Arif <arif15-12301@diu.edu.bd>
2025-02-03 15:44:48 +0600

    wip

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,1 +1,16 @@
-<h4>Hello Category</h4>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Simple web page</title>
+   <link rel = "stylesheet" href = "{{ asset('css/style.css') }}">
+</head>
+<body>
+   <h1> welcome to my web page </h1>
+   <button onclick="changeText()">Click Me</button>
+   <p id="message"></p>
+
+   <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
